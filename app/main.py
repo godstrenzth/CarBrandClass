efe7cf60faf9c0724e278f288a8023f65494288a
GTZ <141386674+godstrenzth@users.noreply.github.com>
2023-10-13 16:51:43 +0700

    Update2.py

--- app/main.py
+++ app/main.py
@@ -1,34 +1,26 @@
-
+import pickle
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-import pickle
-from pydantic import BaseModel
-import numpy as np
 import requests
-import os
-import aiohttp
 from app.code import predictcar
 
-app = FastAPI()
+app = FastAPI() 
+
+m = pickle.load(open(f'model/img_model.pkl', 'rb'))
+#m = pickle.load(open(r'..\model\img_model.pkl', 'rb'))# ..ย้อนไปก่อนทำการโหลด model 
+#gethog = 'http://localhost:5000/api/gethog/'
+# docker to docker
+gethog = 'http://172.17.0.3:80/api/gethog/'
 
 @app.get("/")
 def read_root():
     return {"Class Head Car"}
 
-
-m = pickle.load(open(r'..\model\img_model.pkl', 'rb'))# ..ย้อนไปก่อนทำการโหลด model 
-# gethog = 'http://localhost:8080/api/gethog/'
-# docker to docker
-gethog = 'http://172.17.0.2:80/api/gethog/'
-
 @app.post("/api/carbrand")
 async def genhog(request: Request):
     data = await request.json()
-    async with aiohttp.ClientSession() as session:
-        async with session.get(gethog, json=data) as response:
-            hog_re = await response.json()
-
-    reshead = predictcar(m,[hog_re['HOG Descriptor']])
+    hog1 =requests.get(gethog,json=data)
+    reshead = predictcar(m,hog1.json()['HOG Descriptor'])
     return {"Headcar":reshead}
     
-
+    
