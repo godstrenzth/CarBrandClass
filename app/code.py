05faa6f763146bb33e5aa0f213745e4f07e0d350
GTZ <141386674+godstrenzth@users.noreply.github.com>
2023-10-13 16:52:07 +0700

    Update 21.py

--- app/code.py
+++ app/code.py
@@ -1,6 +1,7 @@
 import pickle
 import json
+import numpy as np
 
 def predictcar(m,HOG):
-    result = m.predict(HOG) # ทำการ predict เพื่อทำนายยี่ห้อรถยนต์
+    result = m.predict(np.array(HOG).reshape(1,-1)) # ทำการ predict เพื่อทำนายยี่ห้อรถยนต์
     return result[0]
